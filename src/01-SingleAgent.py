889a4171b57bac6a35cae55a07836bf98b70a6cb
Tyrone <tvriesde@gmail.com>
2025-01-29 10:11:41 +0000

    update

--- src/01-SingleAgent.py
+++ src/01-SingleAgent.py
@@ -6,7 +6,7 @@ agent = ConversableAgent(
     "chatbot",
     system_message="You are a comedian specialized in telling short story jokes.",
     llm_config=llm_config,
-    human_input_mode="NEVER",  # Never ask for human input.
+    human_input_mode="NEVER",  # Never ask for human input...
 )
 
 reply = agent.generate_reply(
